08e6c6de37d3bfbb0a64e681dfcf8058ae08a0d6
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-07-08 18:28:27 -0400

    delete unnecessary files & add folder

---
+++ Google/course_prereqs.py
@@ -0,0 +1,9 @@
+'''
+As you go to college you have to do some courses but before doing some courses you need to complete some prerequisite courses (Ex- before doing MA202 you need to complete MA201). so the question is you have N courses you have to tell the minimum number of semesters required to complete the whole courses and in one semester you can do any number of courses but before doing a course you have to do all prerequisites of that course. 
+
+And input is given as a 2D vector and in a particular row of the 2D vector, the current course is a prerequisite of the next course.
+
+Ex: {{a, b, c, }, {b, e, d}, {e, f}}.
+
+Suppose I’m taking the first-row {a, b, c} so before doing course “b” you need to complete “a” and before doing to “c” you need to complete “b”.
+'''
