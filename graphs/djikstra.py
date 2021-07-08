d687e9f3a3d005aa6812dbab910a20df380ca7be
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-07-08 18:29:09 -0400

    add graphs

---
+++ graphs/djikstra.py
@@ -0,0 +1,9 @@
+'''
+Dijkstra Algorithm
+
+works on directed/undirected graphs
+- weights must  be non-negative
+- find the lengths of the shortest paths from starting vertex s to all other vertices,
+and return the paths.
+
+'''
