05b539b96aec1c8c38527c7107d79e5f6142ae6f
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-09-09 21:21:54 -0400

    add mst and sssp algorithms

---
+++ graphs/Graph.py
@@ -0,0 +1,192 @@
+from collections import defaultdict, deque
+import heapq
+# define a weighted graph data structure with the following methods:
+
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.neighbors = {}
+
+    def add_nbr(self, nbr, weight=0):
+        if type(nbr) != Node:
+            raise TypeError('Argument must be a Node instance')
+        self.neighbors[nbr] = weight
+
+
+class Graph:
+    def __init__(self, number_of_vertices=0):
+        self.list = defaultdict(list)
+        self.matrix = [[0]*number_of_vertices]*number_of_vertices
+        self.len = number_of_vertices
+        # define the graph as either an adjacency list or adjacency matrix
+        # we’ll use an adjacency list
+
+    def get_len(self):
+        return self.len
+    # method returns the number of vertices in the graph
+
+    def set_edge(self, node1, node2, weight=None):
+        if not weight:
+            if node2 in self.list[node1]:
+                del self.list[node1][node2]
+        else:
+            self.graph[node1][node2] = weight
+            # method adds an edge of weight w between vertices v and u when w is a number;
+            #  if the edge already exists, its weight is updated to w. If w is None then the edge, if it exists, is removed, and if absent remains absent.
+
+    def get_edge(self, node1, node2):
+        if node1 in self.list and node2 in self.list[node1]:
+            return self.list[node1][node2]
+        return -1
+    # method returns the weight of the edge between vertices u and v
+
+    def get_adjacent_vertices(self, node):
+        if node in self.list:
+            return self.list[node]
+        return []
+
+# method returns a list of all vertices that are directly connected to node
+    def get_all_edges(self, node):
+        edges = []
+        for src in self.list:
+            for dest, cost in self.list[node]:
+                edges.append([src, dest])
+        return edges
+
+# method returns a list of all edges in the graph, in unspecified order. For each edge in the graph, it includes only one direction in the list.
+    def sssp(self, node1, node2):
+        def bell_ford():
+            # slower than dijkstra's but handles negative weights(not cycles)
+            # time: 0(EV)
+            in_que = set()
+            distance = defaultdict(lambda: float('inf'))
+            parent = defaultdict(None)
+            distance[node1] = 0
+            parent[node1] = None
+            for v in range(len(self.list)):
+                for node in self.list:
+                    for edge, weight in self.list[node]:
+                        if distance[edge] > distance[node] + weight:
+                            distance[edge] = distance[node] + weight
+                            parent[edge] = node
+
+            return distance  # , parent
+
+        def dijksta():
+            visited = set()
+            distance = defaultdict(lambda: float('inf'))
+            parent = defaultdict(None)  # keep path if necessary
+            distance[node1] = 0
+
+            heap = []
+            heapq.heappush(heap, (0, node1))
+            while len(heap) > 0:
+                (dist, curr_node) = heapq.heappop(heap)
+                if curr_node in visited:
+                    continue
+                visited.add(curr_node)
+
+                for node, weight in self.list[curr_node]:
+                    if node not in visited:
+                        new_weight = min(dist + weight, distance[node])
+                        if new_weight < distance[node]:
+                            distance[node] = new_weight
+                        heapq.heappush(heap, (new_weight, node))
+
+            return distance
+        return [bell_ford(), '\n', dijksta()]
+
+    def mst(self, start_node):
+        def prims():
+            # Time: 0(Elog V) --> we go through all the edges(E) and only add to heaps V vertices(log V)....
+            # not too sure(0(ElogE)possible) since heap may initially have all edges assuming the start node is connected to everything else
+            # Space: 0(max(V,E)), we may have all edges in our heap
+            mst_cost = 0
+            que = []  # deque()
+            heap = []
+            visited = set()
+
+            # push node into min priority heap
+            heapq.heappush(heap, (0, start_node))
+            while len(heap) > 0:
+                # get the vertex with least cost, and delete it from heap
+                (cost, node) = heapq.heappop(heap)
+
+                if node not in visited:  # if not in mst tree, add it
+                    mst_cost += cost
+                    visited.add(node)
+                    que.append((node, cost))
+
+                # for vertices adjacent to popped vertex, add them to heap if not already in mst tree
+                for key, cost in graph.list[node]:
+                    if key in visited:
+                        continue
+                    heapq.heappush(heap, (cost, key))
+                # if we have added all vertices to our mst, no need to explore the rest of the queue
+                if len(visited) == self.len:
+                    return (que, mst_cost)
+
+            return (que, mst_cost)
+
+        def kruskals():
+            # sort all edges by weight(use heap)
+            # pull out in order of increasing weight and add to mst if adding doesn't create a cycle otherwise continue
+            # use union find to check for cycles
+
+            # union find
+            parent = {}
+
+            def find(key):  # find parent
+                if parent[key] == key:
+                    return key
+                return find(parent[key])
+
+            def union(v, w):
+                root_v = find(v)
+                root_w = find(w)
+                parent[root_v] = root_w  # could keep rank to optimize.
+
+            heap = []
+            que = deque()
+            visited = set()
+            que.append(start_node)
+            while que:
+                node = que.pop()
+                parent[node] = node
+                visited.add(node)
+                for (key, cost) in self.list[node]:
+                    if key not in visited:
+                        heapq.heappush(heap, (cost, key, node))
+                        que.append(key)
+
+            mst = []
+            mst_cost = 0
+            while len(mst) < len(visited)-1:
+                (cost, From, To) = heapq.heappop(heap)
+                print(From, To, cost)
+                p_from = find(From)
+                p_to = find(To)
+                if p_from != p_to:
+                    mst.append((From, cost))
+                    mst_cost += cost
+                    union(p_from, p_to)
+            return (mst, mst_cost)
+        print(kruskals())
+        return prims()
+
+
+if __name__ == '__main__':
+    graph = Graph()
+    directed_graph = Graph()
+
+    nodes = [[5, 3, 17], [5, 4, 18], [5, 12, 6], [12, 4, 2], [12, 10, 3], [
+        4, 10, 19], [4, 3, 16], [3, 6, 30], [4, 6, 88], [6, 10, 8]]
+    for a, b, weight in nodes:
+        graph.len += 1
+        graph.list[a].append((b, weight))
+        graph.list[b].append((a, weight))
+        # directed graph for sssp problems
+        directed_graph.list[a].append((b, weight))
+    print(graph.mst(3))
+    print(directed_graph.sssp(5, 6))
