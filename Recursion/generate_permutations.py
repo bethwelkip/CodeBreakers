2f9d693a8ddc84ec85128476c1a99f2272f6f20e
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-10-24 22:05:28 -0400

    generate permutations

--- Recursion/generate_permutations.py
+++ Recursion/generate_permutations.py
@@ -1,25 +1,25 @@
 def generate__permutations(arr):
     res = []
+
     def permutate(start, part=[]):
-        if start + 1 ==len(arr):
+        if start + 1 == len(arr):
             res.append(list(arr))
             return
-        for i in range(start,len(arr)):
+        for i in range(start, len(arr)):
             arr[i], arr[start] = arr[start], arr[i]
             permutate(start+1)
             arr[i], arr[start] = arr[start], arr[i]
-    def permutate_2(start, part=[]):
-        if len(part)==len(arr):
-            res.append(part)
-            return
-        for i in range(start, len(arr)):
-            part.append(arr[start])
-            permutate_2(start+1, list(part))
-            part.pop()
 
-    permutate(0)
+    def permutate_2(arr, part=[]):
+        if len(arr) == 0:
+            res.append(list(part))
+            return
+        for i in range(len(arr)):
+            permutate_2(arr[:i]+arr[i+1:], part + [arr[i]])
+    permutate_2(arr)
     return res
 
-arr = [2,3,5,7]
+
+arr = [2, 3]
 res = generate__permutations(arr)
 print(res)
