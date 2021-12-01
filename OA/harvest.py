8197eb99df5915c58286c11ee6fe8e12de5c9f98
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-12-01 17:13:55 -0500

    efficient solution

--- OA/harvest.py
+++ OA/harvest.py
@@ -20,7 +20,6 @@ def max_profit(k, arr):
             j = (start + i) % n
             z = (j + n//2) % n
             curr += arr[j] + arr[z]
-            print(start, j, z, n)
         max_p = max(curr, max_p)
         start += 1
     return max_p
@@ -28,17 +27,25 @@ def max_profit(k, arr):
 
 def max_profit_efficient(k, arr):
     n = len(arr)
+    
+    total_sum = [0 for _ in range(2*n + 1)]
+    for i in range(n):
+        total_sum[i + 1] = total_sum[i] + arr[i]
+    for i in range(n):
+        total_sum[i + n + 1] = total_sum[i + n] + arr[i]
 
-    prefill = [0 for _ in range(2*n+1)]
-    for i in range(n+1):
-        prefill[i] = arr[i]
+    i, j = 0, len(arr)//2
+    max_profit = float('-inf')
+    while i < (len(arr) // 2):
+        curr_sum = (total_sum[i + k] - total_sum[i]) +(total_sum[i + j + k] - total_sum[i + j])
+        max_profit = max(max_profit, curr_sum)
+        i += 1
 
-        pass
-    pass
+    return max_profit
 
 
 arr = [1, 5, 1, 3, 7, -3]
 arrb = [3, -5]
 arrc = [-6, 3, 6, -3]
-print(max_profit(1, arr))
-print(max_profit(1, arrc))
+# print(max_profit(1, arr))
+print(max_profit_efficient(1, arr))
