8ba0127b13d27f611fc6a522f1345197f5adab27
Bethwel Kiplimo <bkiplimo@princeton.edu>
2021-12-30 14:51:56 -0500

    array products

---
+++ dp/array_product.py
@@ -0,0 +1,35 @@
+
+class Solution:
+    def productExceptCurrentElement(self, arr):
+        '''
+        :type arr: list of int
+        :rtype: list of int
+        '''
+        res = [1]*len(arr)
+        last_idx = len(arr)-1
+
+        pref = [1]*len(arr)
+        pref[0] = arr[0]
+
+        suf = [1]*len(arr)
+        suf[-1] = arr[-1]
+
+        for i in range(1, len(arr)):
+            pref[i] = pref[i-1]*arr[i]
+
+        for i in range(len(arr)-2, -1, -1):
+            suf[i] = suf[i+1]*arr[i]
+
+        for i in range(len(arr)):
+            if i != 0:
+                res[i] *= pref[i-1]
+            if i != last_idx:
+                res[i] *= suf[i+1]
+
+        return res
+
+
+arr = [1, 2, 3]
+sol = Solution()
+res = sol.productExceptCurrentElement(arr)
+print(res)
